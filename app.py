c6ae50f42b3ae55affda3dbd4b4d57e53df5a55e
BoomeshKS <boomeshalone@gmail.com>
2025-12-24 16:16:29 +0530

    Code Edited

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 from flask_cors import CORS
 from config import Config
 from routes.completion_routes import completion_bp
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
+    app = Flask(
+        __name__,
+        static_folder="static",
+        template_folder="templates"
+    )
 
+    app.config.from_object(Config)
     CORS(app)
 
-    # Register blueprints
     app.register_blueprint(completion_bp)
-    
-    @app.errorhandler(404) 
-    def page_not_found(e): 
+
+    @app.errorhandler(404)
+    def page_not_found(e):
         return render_template('404.html'), 404
 
     return app
 
-
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+# 🔥 THIS LINE IS MANDATORY FOR GUNICORN
+app = create_app()
